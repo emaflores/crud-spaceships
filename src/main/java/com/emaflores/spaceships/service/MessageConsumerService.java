80707aa6ab9c4cd7927c29650b17952916491fb4
emanuel flores <emanuelzero@gmail.com>
2024-05-31 10:12:42 -0300

    changed system println to logger

--- src/main/java/com/emaflores/spaceships/service/MessageConsumerService.java
+++ src/main/java/com/emaflores/spaceships/service/MessageConsumerService.java
@@ -3,6 +3,8 @@ package com.emaflores.spaceships.service;
 import com.emaflores.spaceships.config.RabbitMQConfig;
 import com.emaflores.spaceships.entity.MessageLog;
 import com.emaflores.spaceships.repository.MessageLogRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageConsumerService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumerService.class);
+
     @Autowired
     private MessageLogRepository messageLogRepository;
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void receiveMessage(String message) {
-        System.out.println("Received Message: " + message);
+        logger.info("Received Message: {}", message);
 
         MessageLog messageLog = new MessageLog();
         messageLog.setMessage(message);
